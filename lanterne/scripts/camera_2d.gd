e20d6683e37e1ae47db1dcadc6240af3118d29c3
Zacharie Mazeirat <zachariemazeirat@gmail.com>
2026-09-25 13:07:37 +0200

    lvl

--- lanterne/scripts/camera_2d.gd
+++ lanterne/scripts/camera_2d.gd
@@ -1,9 +1,16 @@
 extends Camera2D
 
-
+@export var bounds_rect: ReferenceRect
 @export var shake_fade: float = 10.0
 
 var _shake_strength: float = 0.0
+
+func _ready() -> void:
+	var rect = bounds_rect.get_global_rect()
+	limit_left = int(rect.position.x)
+	limit_top = int(rect.position.y)
+	limit_right = int(rect.position.x + rect.size.x)
+	limit_bottom = int(rect.position.y + rect.size.y)
 
 func trigger_shake(shake) -> void:
 	_shake_strength = shake
