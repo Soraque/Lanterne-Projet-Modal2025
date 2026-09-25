39402cae67499769cdb14480e078e5682c3c54e2
Zacharie Mazeirat <zachariemazeirat@gmail.com>
2026-09-25 13:10:18 +0200

    poule

--- lanterne/scripts/camera_2d.gd
+++ lanterne/scripts/camera_2d.gd
@@ -5,17 +5,32 @@ extends Camera2D
 
 var _shake_strength: float = 0.0
 
-func _ready() -> void:
-	var rect = bounds_rect.get_global_rect()
-	limit_left = int(rect.position.x)
-	limit_top = int(rect.position.y)
-	limit_right = int(rect.position.x + rect.size.x)
-	limit_bottom = int(rect.position.y + rect.size.y)
 
-func trigger_shake(shake) -> void:
+func _ready() -> void:
+	# Si aucun ReferenceRect n'est assigné à la main, on cherche celui du niveau actuel
+	if not bounds_rect:
+		bounds_rect = get_tree().current_scene.get_node_or_null("ReferenceRect") as ReferenceRect
+	
+	_update_limits()
+
+
+func _update_limits() -> void:
+	if bounds_rect:
+		var rect = bounds_rect.get_global_rect()
+		limit_left = int(rect.position.x)
+		limit_top = int(rect.position.y)
+		limit_right = int(rect.position.x + rect.size.x)
+		limit_bottom = int(rect.position.y + rect.size.y)
+
+
+func trigger_shake(shake: float) -> void:
 	_shake_strength = shake
+
 
 func _process(delta: float) -> void:
 	if _shake_strength > 0:
 		_shake_strength = lerp(_shake_strength, 0.0, shake_fade * delta)
-		offset = Vector2(randf_range(-_shake_strength, _shake_strength), randf_range(-_shake_strength, _shake_strength))
+		offset = Vector2(
+			randf_range(-_shake_strength, _shake_strength),
+			randf_range(-_shake_strength, _shake_strength)
+		)
